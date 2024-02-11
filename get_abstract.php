0c5a1d6d5127f97fca593245753f205b2292933e
yuki2021 <ordinary.plusminus1@gmail.com>
2024-02-11 18:04:16 +0900

    要約取得のフロントエンドをjson対応に変えたので修正

--- get_abstract.php
+++ get_abstract.php
@@ -8,10 +8,7 @@ class GetAbstract {
     private $dbConfig;
 
     public function __construct() {
-
         $this->dbConfig = $GLOBALS['dbConfig'];
-
-        // DB接続クラス取得
         $this->db = new \Hadi\Database();
         $this->db->connect($this->dbConfig);
     }
@@ -20,53 +17,45 @@ class GetAbstract {
         $this->db->disconnect();
     }
 
-    /// 渡されたURLでDBを検索する。見つけたデータを返す
     public function searchDB($url) {
-        // APCuにキャッシュがあればそれを返す
         $data = apcu_fetch($url);
         if ($data === false) {
             $sql = 'SELECT b.abstract FROM `hatena_blog_data` AS a 
             LEFT JOIN `hatena_content_abstract` AS b 
             ON a.`id` = b.`hatena_blog_id`
-            where b.`abstract` IS NOT NULL AND
+            WHERE b.`abstract` IS NOT NULL AND
             a.`url` = :url
-            order by a.`published` desc';
-            
-            //prepareによるクエリの実行準備
+            ORDER BY a.`published` DESC';
+
             $sth = $this->db->pdo->prepare($sql);
-
-            //検索クエリの設定
-            $sth -> bindValue(':url', $url);
-            
-            //検索クエリの実行
-            $sth -> execute();
-
-            //結果を配列で取得
-            $data = $sth -> fetch(PDO::FETCH_ASSOC);
-
-            // APCuにキャッシュする
-            apcu_store($url, $data, 60 * 60 * 24);
+            $sth->bindValue(':url', $url);
+            $sth->execute();
+            $data = $sth->fetch(PDO::FETCH_ASSOC);
+            apcu_store($url, $data, 60 * 60 * 24); // 1日キャッシュ
         }
 
         return $data;
     }
 
-    /// 見つけたデータをJSONとして送り返す
     public function sendJSON($data) {
-        header('Access-Control-Allow-Origin:https://www.ituki-yu2.net');
-        header("Content-type: text/xml;charset=utf-8");
-        if(!empty($data['abstract'])) {
-            echo json_encode(array('result' => $data['abstract']),
-                JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
-        } else {
-            echo json_encode(array('error' => 'error'),
-                JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
-        }
+        header('Access-Control-Allow-Origin: https://www.ituki-yu2.net');
+        header('Access-Control-Allow-Headers: Content-Type');
+        header('Content-Type: application/json');
+        echo json_encode($data ? ['result' => $data['abstract']] : ['error' => 'No data found'],
+            JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
     }
 
-    /// POST変数からURLを抜き出して文字列で返す
     public function getPOST() {
-        $url = $_POST['url'];
+        // JSONデータの取得とデコード
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true); // trueで連想配列に変換
+        $url = $data['url'] ?? '';
+
+        if (!preg_match('/^https:\/\/www\.ituki-yu2\.net\/entry\/.*/', $url)) {
+            $this->sendJSON(['error' => 'Invalid URL format']);
+            exit;
+        }
+
         return $url;
     }
 }
